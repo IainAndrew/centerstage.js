c9f607671ccaf07e7614a359d208ef651957aefa
Iain Andrew <iainbruceandrew@gmail.com>
2014-05-17 23:23:38 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var $container = $('#hovercont'),
+    $items = $container.children('.hover-item');
+ 
+$items.on( 'mouseenter', function(e) {  
+    var $item = $(this);
+    $(function() { 
+        if( $item.hasClass('active') ) return false;
+        $items.not($item).removeClass('active').addClass('inactive');
+        $item.removeClass('inactive').addClass('active');
+    });
+});
+ 
+$container.on( 'mouseleave', function(e) {
+    $items.removeClass('active inactive');
+});
